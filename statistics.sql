542107db1f1570aade88a61ab61ad3a0fb6901d7
moluoX <minaxu01@126.com>
2022-12-12 11:44:30 +0800

    1

--- statistics.sql
+++ statistics.sql
@@ -1,5 +1,5 @@
 select a.NewName,b.DongNo,c.Num,c.* 
 from [dbo].[LouPans] a join [dbo].[LouDongs] b on a.Id=b.LouPanId
 join [dbo].[TaoFangs] c on b.Id=c.LouDongId
-where c.SaleStatusChangeTime>GETDATE()-7
+where c.SaleStatusChangeTime>GETDATE()-0.5
 order by a.NewName, b.DongNo, c.Num
